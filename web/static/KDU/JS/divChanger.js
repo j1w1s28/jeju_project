51e7d37097b8c340e46572cc7540da59311e7822
WeWishWell <bigdata0418@gmail.com>
2022-08-02 17:58:33 +0900

    애증의 모달

--- web/static/KDU/JS/divChanger.js
+++ web/static/KDU/JS/divChanger.js
@@ -12,15 +12,11 @@ function mapChanger(divWidth) {
             //header 중앙정렬
             document.querySelector('#headerBody').style.marginLeft = CustomMargin;
             document.querySelector('#headerBody').style.marginRight = CustomMargin;
-            //modal 중앙정렬
-            document.querySelector('.modal').style.left = '50%';
         } else {
             //메인 DIV 보다 화면이 작을 때
             document.querySelector('#baseDiv').style.marginLeft = lmargin + 'px';
             //메인DIV 폭보다 화면이 작을 때 header
             document.querySelector('#headerBody').style.marginLeft = lmargin + 'px';
-            //modal 중앙정렬
-            document.querySelector('.modal').style.left = '720px';
         }
     } catch (error) {
         // console.log(error);
